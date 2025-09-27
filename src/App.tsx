f8ad1a50a4ed12a570868591465d8d164dbc902b
Lovivo Restore <restore@lovivo.ai>
2025-09-26 19:39:54 -0600

    [lovivo-restore] Restore to message 785c39aa-e0ad-4255-a7a4-ad6f99f83238
    store: 91aba598-309c-4c84-9472-b26fcf568451
    conv: 7fb3b2ce-fd1a-4686-af54-168cb8c9caf8
    msg: 785c39aa-e0ad-4255-a7a4-ad6f99f83238
    restored_from: 98a300fc9b88d819dfe7ad6e53c6c996151be694

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,49 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { CartProvider } from '@/contexts/CartContext'
-import { CartUIProvider } from '@/components/CartProvider'
-import { SettingsProvider } from '@/contexts/SettingsContext'
-import { Toaster } from '@/components/ui/toaster'
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { trackPageView } from "@/lib/tracking-utils";
+import { CartProvider } from "@/contexts/CartContext";
+import { CartUIProvider } from "@/components/CartProvider";
+import { SettingsProvider } from "@/contexts/SettingsContext";
+import { PixelProvider } from "@/contexts/PixelContext";
+import { FaviconManager } from "@/components/FaviconManager";
+import Index from "./pages/Index";
+import Product from "./pages/Product";
+import Blog from "./pages/Blog";
+import BlogPost from "./pages/BlogPost";
+import NotFound from "./pages/NotFound";
+import Checkout from "./pages/Checkout";
+import ThankYou from "./pages/ThankYou";
+import Cart from "./pages/Cart";
 
-// Pages
-import Index from '@/pages/Index'
-import Product from '@/pages/Product'
-import Cart from '@/pages/Cart'
-import Checkout from '@/pages/Checkout'
-import ThankYou from '@/pages/ThankYou'
-import Blog from '@/pages/Blog'
-import BlogPost from '@/pages/BlogPost'
-import About from '@/pages/About'
-import ShippingCalculator from '@/pages/ShippingCalculator'
-import NotFound from '@/pages/NotFound'
+const queryClient = new QueryClient();
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 5, // 5 minutes
-      retry: 1,
-    },
-  },
-})
+// Component to track page views on route changes
+function PageViewTracker() {
+  const location = useLocation();
+  
+  useEffect(() => {
+    trackPageView();
+  }, [location.pathname]);
+  
+  return null;
+}
 
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <SettingsProvider>
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <SettingsProvider>
+      <PixelProvider>
+        <FaviconManager />
         <CartProvider>
-          <CartUIProvider>
-            <Router>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <CartUIProvider>
+              <PageViewTracker />
               <Routes>
                 <Route path="/" element={<Index />} />
                 <Route path="/products/:slug" element={<Product />} />
@@ -41,17 +52,16 @@ function App() {
                 <Route path="/thank-you/:orderId" element={<ThankYou />} />
                 <Route path="/blog" element={<Blog />} />
                 <Route path="/blog/:slug" element={<BlogPost />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/shipping-calculator" element={<ShippingCalculator />} />
+                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
                 <Route path="*" element={<NotFound />} />
               </Routes>
-            </Router>
-            <Toaster />
-          </CartUIProvider>
+            </CartUIProvider>
+          </BrowserRouter>
+        </TooltipProvider>
         </CartProvider>
-      </SettingsProvider>
-    </QueryClientProvider>
-  )
-}
+      </PixelProvider>
+    </SettingsProvider>
+  </QueryClientProvider>
+);
 
-export default App
+export default App;
